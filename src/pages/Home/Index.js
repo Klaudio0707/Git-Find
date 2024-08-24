abe17c5d1ae2b60b8d1ff5b1ff78a5f6b24f47ea
Cláudio Roberto <claudioroberto0707@outlook.com>
2024-08-24 10:40:17 -0300

    conclusão do projeto gitFind

--- src/pages/Home/Index.js
+++ src/pages/Home/Index.js
@@ -1,10 +1,73 @@
+import { useState } from "react";
 import { Header } from "../../components/Header";
+import background from "../../assets/background_.png";
+import profile from "../../assets/perfil.png";
+import  ItemList  from "../../components/ItemList";
+
+
+import "./styles.css";
 
 function App() {
+  const [user, setUser] = useState("");
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repos, setRepos] = useState(null);
+
+const handleGetData = async () => {
+  const userData = await fetch(`https://api.github.com/users/${user}`);
+  const newUser = await userData.json();
+
+
+if(newUser.name) {
+  const {avatar_url, name, bio, login} = newUser;
+setCurrentUser({avatar_url, name, bio, login});
+
+const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+
+const newRepos = await reposData.json();
+
+if(newRepos.length){
+
+  setRepos(newRepos);
+
+}
+}
+};
+
+
   return (
     <div className="App">
      <Header />
-    </div>
+<div className="conteudo">
+<img src={background} className="background" alt="img background"/>
+<div className="info">
+<div>
+<input name="usuario" value={user} onChange={(event) => setUser(event.target.value)} placeholder="@username" />
+<button onClick={handleGetData}>Buscar</button>
+</div>
+{currentUser?.name ? (<>
+  <div className="perfil">
+<img src={currentUser.avatar_url} className="profile" alt="foto perfil" />
+<div>
+<h3>{currentUser.name}</h3>
+<span>@{currentUser.login}</span>
+<p>{currentUser.bio}</p>
+
+</div>
+</div>
+<hr></hr>
+</>): null}
+{repos?.length ? (
+  <div>
+
+    <h4 className="repositorio">Repositórios: </h4>
+  {repos.map(repo => (
+<ItemList title={repo.name} description={repo.description} />
+  ))};
+</div>
+) : null}
+</div>
+</div>
+</div>
   );
 }
 
